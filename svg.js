0b417bb8bbcdc87db8f881535b7116490e87f585
Sara <a20sarle@student.his.se>
2023-03-10 11:27:33 +0100

    svg, init chart

--- svg.js
+++ svg.js
@@ -1,1 +1,15 @@
-alert();
+var options = {
+    chart: {
+        type: 'line'
+    },
+    series: [{
+        name: 'Values',
+        data: [30,40,45,50,49,60,70]
+    }],
+    xaxis: {
+        categories: ["Arctic", "North Atlantic", "South Atlantic", "Indian", "North Pacific", "South Pacific", "Southern"]
+    }
+}
+
+var chart = new ApexCharts(document.querySelector("#myChart"), options);
+chart.render();
