875ae78ee2c5b17a2fdc43163319a86a8b2db5cd
Sara <a20sarle@student.his.se>
2023-03-10 11:35:25 +0100

    canvas js clean up

--- canvas.js
+++ canvas.js
@@ -2,9 +2,7 @@
 var chartArea = document.getElementById('myChart');
 
 // Setup - sets values for labels and datapoints
-const labels = ["Arctic", "North Atlantic", "South Atlantic",
-    "Indian", "North Pacific", "South Pacific",
-    "Southern"];
+const labels = ["Arctic", "North Atlantic", "South Atlantic", "Indian", "North Pacific", "South Pacific", "Southern"];
 const datapoints = [30,40,45,50,49,60,70];
 
 // Config - used to change how the chart behaves
