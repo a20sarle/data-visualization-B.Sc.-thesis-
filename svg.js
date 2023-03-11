04f24106f6be426b2de84a2666c4dab66630a33a
Sara <a20sarle@student.his.se>
2023-03-11 19:35:57 +0100

    svg, data from internal json

--- svg.js
+++ svg.js
@@ -1,13 +1,46 @@
+var jsonfile = {
+    "jsonarray":
+    [{
+        "label": "Arctic",
+        "temp": 30
+    }, {
+        "label": "North Atlantic",
+        "temp": 40
+    }, {
+        "label": "South Atlantic",
+        "temp": 45
+    },{
+        "label": "Indian",
+        "temp": 50
+    },{
+        "label": "North Pacific",
+        "temp": 49
+    },{
+        "label": "South Pacific",
+        "temp": 60
+    },{
+        "label": "Southern",
+        "temp": 70
+    }]
+};
+
+const labels = jsonfile.jsonarray.map(function(e) {
+    return e.label;
+ });
+const datapoints = jsonfile.jsonarray.map(function(e) {
+    return e.temp;
+ });
+
 var options = {
     chart: {
         type: 'line'
     },
     series: [{
         name: 'Values',
-        data: [30,40,45,50,49,60,70]
+        data: datapoints
     }],
     xaxis: {
-        categories: ["Arctic", "North Atlantic", "South Atlantic", "Indian", "North Pacific", "South Pacific", "Southern"]
+        categories: labels
     }
 }
 
