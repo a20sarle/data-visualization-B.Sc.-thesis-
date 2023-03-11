8afa80b91b8091bdbe1c1105eac20472ba88ed93
Sara <a20sarle@student.his.se>
2023-03-11 19:14:42 +0100

    canvas, data from internal json

--- canvas.js
+++ canvas.js
@@ -1,9 +1,39 @@
+var jsonfile = {
+    "jsonarray":
+    [{
+        "label": "Arctic",
+        "temp": 30
+    }, {
+        "label": "North Atlantic",
+        "temp": 40
+    }, {
+        "label": "South Atlantic",
+        "temp": 45
+    },{
+        "label": "Indian",
+        "temp": 50
+    },{
+        "label": "North Pacific",
+        "temp": 49
+    },{
+        "label": "South Pacific",
+        "temp": 60
+    },{
+        "label": "Southern",
+        "temp": 70
+    }]
+};
+
 // Get Canvas element from external HTML file through id
 var chartArea = document.getElementById('myChart');
 
 // Setup - sets values for labels and datapoints
-const labels = ["Arctic", "North Atlantic", "South Atlantic", "Indian", "North Pacific", "South Pacific", "Southern"];
-const datapoints = [30,40,45,50,49,60,70];
+const labels = jsonfile.jsonarray.map(function(e) {
+    return e.label;
+ });
+const datapoints = jsonfile.jsonarray.map(function(e) {
+    return e.temp;
+ });
 
 // Config - used to change how the chart behaves
 const dataObj = {
