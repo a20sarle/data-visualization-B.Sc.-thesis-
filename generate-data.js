e1721ce4048735534f700d006576e2df72597e1d
Sara <a20sarle@student.his.se>
2023-03-15 16:29:44 +0100

    generate data label w format xMMM-DD

--- generate-data.js
+++ generate-data.js
@@ -5,7 +5,20 @@ const maxValue = 10;
 const anomaliesFinalArray = [];
 
 for (let n = minValue; n <= maxValue; n += 1.00) {
-    anomaliesFinalArray.push({ label: n, temp: n });
+
+    // Randomize a title on format xMMM-DD
+
+    // Ex. "Thu Jul 29 1999 05:36:14 GMT+0200 (Central European Summer Time)"
+    var longDate = new Date(new Date() - Math.random()*(1e+12));
+
+    // Ex. "Jul" instead of returning 7 (from Thu Jul 29).
+    var formatMonth = new Intl.DateTimeFormat("en-US", { month: "short" }).format;
+    var monthShort = formatMonth(longDate);
+
+    // Ex. "29" (from Thu Jul 29).
+    var dateShort = longDate.getDate();
+
+    anomaliesFinalArray.push({ label: "x"+monthShort+"-"+dateShort, temp: n });
 }
 
 console.log(anomaliesFinalArray);
