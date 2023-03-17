769ccddb9e3efdcbf75475decf0a3170598c34d5
Sara <a20sarle@student.his.se>
2023-03-17 18:17:43 +0100

    generate temperatures min -1.0448953 max 1.2235882

--- generate-data.js
+++ generate-data.js
@@ -4,6 +4,11 @@ const minValue = 1;
 const maxValue = 10;
 const anomaliesFinalArray = [];
 
+function getTemperature(min, max) {
+    var temp = Math.random() * (max - min) + min;
+    return temp.toFixed(7);
+}
+
 for (let n = minValue; n <= maxValue; n += 1.00) {
 
     // Randomize a title on format xMMM-DD
@@ -18,7 +23,9 @@ for (let n = minValue; n <= maxValue; n += 1.00) {
     // Ex. "29" (from Thu Jul 29).
     var dateShort = longDate.getDate();
 
-    anomaliesFinalArray.push({ label: "x"+monthShort+"-"+dateShort, temp: n });
+    var temparature = getTemperature(-1.0448954, 1.2235883);
+
+    anomaliesFinalArray.push({ label: "x"+monthShort+"-"+dateShort, temp: temparature });
 }
 
 console.log(anomaliesFinalArray);
