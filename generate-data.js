72cde7498a08df60e4132412773f2bf88e63c7b3
Sara <a20sarle@student.his.se>
2023-03-17 18:23:37 +0100

    generate data split code getLabel moved new func

--- generate-data.js
+++ generate-data.js
@@ -9,23 +9,27 @@ function getTemperature(min, max) {
     return temp.toFixed(7);
 }
 
-for (let n = minValue; n <= maxValue; n += 1.00) {
-
-    // Randomize a title on format xMMM-DD
+function getLabel() {
 
     // Ex. "Thu Jul 29 1999 05:36:14 GMT+0200 (Central European Summer Time)"
     var longDate = new Date(new Date() - Math.random()*(1e+12));
 
-    // Ex. "Jul" instead of returning 7 (from Thu Jul 29).
+    // Ex. "Jul" instead of 7 (from Thu Jul 29).
     var formatMonth = new Intl.DateTimeFormat("en-US", { month: "short" }).format;
     var monthShort = formatMonth(longDate);
 
     // Ex. "29" (from Thu Jul 29).
     var dateShort = longDate.getDate();
 
+    return "x"+monthShort+"-"+dateShort;
+}
+
+for (let n = minValue; n <= maxValue; n += 1.00) {
+
+    var date = getLabel();
     var temparature = getTemperature(-1.0448954, 1.2235883);
 
-    anomaliesFinalArray.push({ label: "x"+monthShort+"-"+dateShort, temp: temparature });
+    anomaliesFinalArray.push({ label: date, temp: temparature });
 }
 
 console.log(anomaliesFinalArray);
