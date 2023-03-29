5b681d106fbca4b17071d0067e83b9bf6f3074eb
Sara <a20sarle@student.his.se>
2023-03-29 11:58:45 +0200

    tm, seed added and double checked

--- tampermonkey.js
+++ tampermonkey.js
@@ -5,6 +5,7 @@
 // @description  Measure script for interactive performance.
 // @author       You
 // @match        http://localhost/<path>
+// @require      https://chancejs.com/chance.min.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.
 // @grant        none
 // @run-at       document-idle
@@ -14,6 +15,9 @@
     'use strict';
 
     // Your code here...
+    var seed = 1;
+    var chance;
+    var checkChance;
 
     // setIntervalX is used as setInterval is predefined.
     function setIntervalX(callback, delay, repetitions) {
@@ -30,11 +34,22 @@
     // Call myFunction every 2 sec but only 5 times.
     // 2000 = delay, 5 = repetitions
     setIntervalX(function(){
-        myFunction();
+        myFunction(seed);
+        seed++;
     }, 2000, 5);
 
     // Print current time to console.
-    function myFunction() {
+    function myFunction(s) {
         console.log("Current time: " + new Date().toLocaleTimeString());
+
+        chance = new Chance(s);
+        chance = chance.integer({ min: 1, max: 3 });
+        console.log(chance);
+
+        // Checks that seed (s) works, meaning that `chance`
+        // and `heckChance` return the same values.
+        checkChance = new Chance(s);
+        checkChance = checkChance.integer({ min: 1, max: 3 });
+        console.log(checkChance+" <-- check");
     }
 })();
