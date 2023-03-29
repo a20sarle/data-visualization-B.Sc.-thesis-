15fa34e3e3c3b84a74af0f8260f640083bc0ee18
Sara <a20sarle@student.his.se>
2023-03-29 11:51:12 +0200

    tm, call func every 2 sec but only 5 times

--- tampermonkey.js
+++ tampermonkey.js
@@ -15,8 +15,23 @@
 
     // Your code here...
 
-    // Call myFunction every 2 sec. Use clearInterval(t); to stop.
-    var t = setInterval(myFunction, 2000);
+    // setIntervalX is used as setInterval is predefined.
+    function setIntervalX(callback, delay, repetitions) {
+        let x = 0;
+        let intervalID = setInterval(function () {
+            callback();
+            if (++x === repetitions) {
+                // Used to stop call myFunction.
+                clearInterval(intervalID);
+            }
+        }, delay);
+    }
+
+    // Call myFunction every 2 sec but only 5 times.
+    // 2000 = delay, 5 = repetitions
+    setIntervalX(function(){
+        myFunction();
+    }, 2000, 5);
 
     // Print current time to console.
     function myFunction() {
