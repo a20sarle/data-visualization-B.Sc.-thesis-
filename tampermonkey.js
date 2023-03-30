a08003769e0934d0f1ac019bce56f03e9086baf8
Sara <a20sarle@student.his.se>
2023-03-30 12:53:32 +0200

    tm, returned to the none reloading of page solution

--- tampermonkey.js
+++ tampermonkey.js
@@ -14,33 +14,33 @@
 (function () {
     'use strict';
 
-    // Declare init value of variables first time the code is run
-    var repeated = window.localStorage.getItem("repeated");
-    if(repeated === null)
-    {
-        window.localStorage.setItem("repeated", 0);
-        window.localStorage.setItem("seed", 1);
-        window.localStorage.setItem("chance", 0);
-        window.localStorage.setItem("prevChance", 3);
-        window.localStorage.setItem("measurements", "");
-     }
+    // Your code here...
+    var seed = 1;
+    var prevChance = 3;
+    var chance;
+    const measurements = [];
 
-     repeated = window.localStorage.getItem("repeated");
-     var seed = window.localStorage.getItem("seed");
-     var chance = window.localStorage.getItem("chance");
-     var prevChance = window.localStorage.getItem("prevChance");
-     var measurements = window.localStorage.getItem("measurements");
-
-     repeated++;
-
-    if(repeated < 6){
-        myFunction(seed);
-        localStorage.setItem('repeated', repeated);
-        location.reload();
+    // setIntervalX is used as setInterval is predefined.
+    function setIntervalX(callback, delay, repetitions) {
+        let x = 0;
+        let intervalID = setInterval(function () {
+            callback();
+            if (++x === repetitions) {
+                // Used to stop call myFunction.
+                clearInterval(intervalID);
+            }
+        }, delay);
     }
+
+    // Call myFunction every 2 sec but only 5 times.
+    // 2000 = delay, 5 = repetitions
+    setIntervalX(function(){
+        myFunction(seed);
+    }, 2000, 5);
 
     // Print current time to console.
     function myFunction(s) {
+        var clock = new Date().toLocaleTimeString();
 
         do {
             chance = new Chance(s);
@@ -62,22 +62,16 @@
         var renderTime = (end - start).toFixed(2);
         console.log(chance+", render: "+renderTime);
 
-        measurements += '{"btn": "'+chance+'","render": "'+renderTime+'"},';
+        measurements.push({ btn: chance, render: renderTime });
 
         prevChance = chance;
-
-        window.localStorage.setItem("repeated", repeated);
-        window.localStorage.setItem("seed", seed);
-        window.localStorage.setItem("chance", chance);
-        window.localStorage.setItem("prevChance", prevChance);
-        window.localStorage.setItem("measurements", measurements);
     }
 
     // Download generated data
     var downloadBtn = document.getElementById('saveData');
     downloadBtn.setAttribute("download", "measurements.json");
     downloadBtn.onclick = function() {
-        var measurementData = "data:text/json;charset=utf-8," + encodeURIComponent("["+measurements.slice(0, -1)+"]",undefined,2);
+        var measurementData = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(measurements,undefined,2));
         downloadBtn.setAttribute("href", measurementData);
     };
 })();
